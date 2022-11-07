aabd06244a89464dd45d0511cdd968cc6019ff59
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-07 19:28:19 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -25,22 +25,20 @@ class Square():
         """Width setter"""
         if value < 1:
             raise ValueError("Width must be greater than 0")
-        else:
-            self.__width = value
+        self.__width = value
     
     @height.setter
     def height(self, value):
         """Height setter"""
         if value < 1:
             raise ValueError("Height must be greater than 0")
-        else:
-            self.__height = value
+        self.__height = value
     
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
-    def  perimeter_of_my_square(self):
+    def  permiter_of_my_square(self):
         """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -53,4 +51,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
