5d8671c38687dde6bcbef41e62ea8a92e0dd0d4e
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-07 19:40:14 +0100

    Update app.py

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
