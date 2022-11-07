76e86917b2ceae467fa6beb9743150be9e11cde1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-07 19:17:41 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,8 +7,8 @@ class Square():
 
     def __init__(self, width=0, height=0):
         """ init method """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         
     @property
     def width(self):
